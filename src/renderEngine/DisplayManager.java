06d2bf453e8c6ef6b800bbd342e95f19eb8d1006
Viktor <viktorkostraba@gmail.com>
2016-07-20 00:11:01 +0300

    VAOs and VBOs loader , model , render

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -1,43 +1,39 @@
 package renderEngine;
 
 import org.lwjgl.LWJGLException;
-import org.lwjgl.opengl.ContextAttribs;
-import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.PixelFormat;
+import org.lwjgl.opengl.*;
 
 public class DisplayManager {
-	private static int WIDTH = 1280;
-	private static int HEIGHT = 720;
-	private static int FPS_CAP = 120;
+    private static int WIDTH = 1280;
+    private static int HEIGHT = 720;
+    private static int FPS_CAP = 120;
 
-	public static void createDisplay() {
+    public static void createDisplay() {
 
-		ContextAttribs attribs = new ContextAttribs(3, 2);
-		attribs.withForwardCompatible(true);
-		attribs.withProfileCore(true);
+        ContextAttribs attribs = new ContextAttribs(3, 2);
+        attribs.withForwardCompatible(true);
+        attribs.withProfileCore(true);
 
-		try {
-			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-			Display.create(new PixelFormat(), attribs);
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-		}
+        try {
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+            Display.create(new PixelFormat(), attribs);
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
 
-		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+        GL11.glViewport(0, 0, WIDTH, HEIGHT);
 
-	}
+    }
 
-	public static void updateDisplay() {
-		Display.sync(FPS_CAP);
-		Display.update();
+    public static void updateDisplay() {
+        Display.sync(FPS_CAP);
+        Display.update();
 
-	}
+    }
 
-	public static void closeDisplay() {
-		Display.destroy();
+    public static void closeDisplay() {
+        Display.destroy();
 
-	}
+    }
 
 }
--- src/testEngine/MainGameLoop.java
+++ src/testEngine/MainGameLoop.java
@@ -1,24 +1,46 @@
 package testEngine;
 
 import org.lwjgl.opengl.Display;
-
 import org.lwjgl.opengl.GL11;
 import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Render;
 
 public class MainGameLoop {
 
-	public static void main(String[] args) {
-		DisplayManager.createDisplay();
-		System.out.println("OpenGL version: " + GL11.glGetString(GL11.GL_VERSION));
+    public static void main(String[] args) {
 
-		while (!Display.isCloseRequested()) {
+        DisplayManager.createDisplay();
+        System.out.println("OpenGL version: " + GL11.glGetString(GL11.GL_VERSION));
+        Loader loader = new Loader();
+        Render render = new Render();
+        float[] vertices = {
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
 
-			DisplayManager.updateDisplay();
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f,
+        };
+        RawModel model = loader.loadToVAO(vertices);
 
-		}
 
-		DisplayManager.closeDisplay();
 
-	}
+        while (!Display.isCloseRequested()) {
+
+            render.prepare();
+
+            render.render(model);
+
+            DisplayManager.updateDisplay();
+
+        }
+
+        loader.cleatUp();
+        DisplayManager.closeDisplay();
+
+    }
 
 }
