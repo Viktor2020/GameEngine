92e5dce5b69c0f50973abd51ee718ee2a2b46e40
Viktor <viktorkostraba@gmail.com>
2016-07-26 00:27:21 +0300

    added rotation skybox

--- src/skybox/SkyboxShader.java
+++ src/skybox/SkyboxShader.java
@@ -3,17 +3,20 @@ package skybox;
 import entities.Camera;
 import org.lwjgl.util.vector.Matrix4f;
 import org.lwjgl.util.vector.Vector3f;
+import renderEngine.DisplayManager;
 import shaders.ShaderProgram;
 import toolbox.Maths;
 
 public class SkyboxShader extends ShaderProgram {
 
+    private static final float ROTATION_SPEED = 0.1f;
     private static final String VERTEX_FILE = "src/skybox/skyboxVertexShader.txt";
     private static final String FRAGMENT_FILE = "src/skybox/skyboxFragmentShader.txt";
-
     private int locationProjectionMatrix;
     private int locationViewMatrix;
     private int locationFogColour;
+
+    private float rotation = 0;
 
     public SkyboxShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
@@ -28,6 +31,8 @@ public class SkyboxShader extends ShaderProgram {
         matrix.m30 = 0;
         matrix.m31 = 0;
         matrix.m32 = 0;
+        rotation += ROTATION_SPEED * DisplayManager.getFrameTimeSeconds();
+        Matrix4f.rotate((float) Math.toRadians(rotation), new Vector3f(0, 1, 0), matrix, matrix);
         super.loadMatrix(locationViewMatrix, matrix);
     }
 
